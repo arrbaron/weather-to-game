0c7b981d69ff8f505a92072d0d4dc9d3b0cb8d72
Roy Baron <baron.roy@gmail.com>
2017-10-25 13:40:55 -0400

    get weather data for search term

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// weatherbit API key: 62362ac75c5948fc9871e28bb51d0d19
-
 const WEATHERBIT_CURRENT_URL = "https://api.weatherbit.io/v2.0/current";
 const WEATHERBIT_EXTENDED_URL = "https://api.weatherbit.io/v2.0/forecast/daily";
 
@@ -7,6 +5,20 @@ const App = {
     reset: function() {
         EventListeners.startListeners();
         HTMLRenderer.showIntro();
+    },
+
+    search: function(query) {
+        this.getDataFromAPI(query, HTMLRenderer.showDayForecast);
+    },
+
+    getDataFromAPI: function(searchTerm, callback) {
+        let location = searchTerm;
+        const query = {
+            key: "62362ac75c5948fc9871e28bb51d0d19",
+            units: "I",
+            city: location
+        };
+        $.getJSON(WEATHERBIT_CURRENT_URL, query, callback).fail(HTMLRenderer.showErr);
     }
 };
 
@@ -20,7 +32,7 @@ const EventListeners = {
             this.handleHeaderLinkClicked();
             this.handleDayForecastLinkClicked();
             
-            listenersStarted = true;
+            this.listenersStarted = true;
         }
     },
 
@@ -36,13 +48,12 @@ const EventListeners = {
             const queryTarget = $(this).find(".intro__query");
             this.searchQuery = queryTarget.val();
 
-            // App.search(searchQuery);
-            HTMLRenderer.showDayForecast();
+            App.search(this.searchQuery);
             queryTarget.val("");
         });
     },
 
-    handleDayForecastLinkClicked() {
+    handleDayForecastLinkClicked: function() {
         $(".day-forecast__link").click(function(event) {
             HTMLRenderer.showExtendedForecast();
         });
@@ -56,16 +67,24 @@ const HTMLRenderer = {
         $(".extended-forecast").addClass("hidden");
     },
 
-    showDayForecast: function() {
+    showDayForecast: function(data) {
         $(".intro").addClass("hidden");
         $(".day-forecast").removeClass("hidden");
         $(".extended-forecast").addClass("hidden");
+
+        console.log(data);
     },
 
     showExtendedForecast: function() {
         $(".intro").addClass("hidden");
         $(".day-forecast").addClass("hidden");
         $(".extended-forecast").removeClass("hidden");
+
+        alert("showing extended forecast");
+    },
+
+    showErr: function() {
+        alert("error");
     }
 };
 
